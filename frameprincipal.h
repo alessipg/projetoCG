eb887c53d3d88379a6a60ba79d2782ee50e1458e
Gabriel Alessi <gabriel.alessip@gmail.com>
2025-01-20 14:37:00 -0300

    comentarios no codigo

--- frameprincipal.h
+++ frameprincipal.h
@@ -29,26 +29,23 @@ class FramePrincipal : public QFrame
 {
     Q_OBJECT
 public:
-    explicit FramePrincipal(QWidget *parent = nullptr);
-    void paintEvent(QPaintEvent *event) override;
-    DisplayFile df;
-    Window *window;
-    void processarEntrada(const QString inputText);
-    void centralizarWindow();
-    void corrigirRotacaoWindow(Window *inputWindow);
-    void calcularCoordenadasViewPort(Window *inputWindow);
-    void escalonarPontos();
-    void pipeline();
-    void ajustarPosicionamento();
-    //salva o objeto que está desenhado para aplicar a mudança de posição
-    //provavelmente quando trabalharmos com mais de um obj na cena isso não funcionará
+    bool isPerspectiva;
     Objeto *objCentro;
     Objeto *objAtual;
-    bool isPerspectiva;
+    Window *window;
+    DisplayFile df;
+    void pipeline();
+    void escalonarPontos();
+    void centralizarWindow();
+    void ajustarPosicionamento();
     Objeto* getObjetoByName(const QString& nome);
+    void paintEvent(QPaintEvent *event) override;
+    void processarEntrada(const QString inputText);
+    void corrigirRotacaoWindow(Window *inputWindow);
+    void calcularCoordenadasViewPort(Window *inputWindow);
+    explicit FramePrincipal(QWidget *parent = nullptr);
     std::tuple<double, double, double> calcularAngulos(Ponto p, Ponto vUp, Ponto centro);
 public slots:
-    //void transformarWindow(const QString &inputText);
     void transformarObjeto(char op,double v1,double v2,double v3,char eixo, char centro);
 };
 
